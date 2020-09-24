a5309b7e9b2bd4e5a56a79a9330f45112f5346f5
Rimma Stolina <rim2290@mail.ru>
2020-09-24 19:14:09 +0300

    src added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int paymentOneYear = (int) service.calculate(1_000_000, 12, 0.0999);
+        long s=1000000;
+        int t=12;
+        double p=0.0999;
+        int paymentOneYear = (int) service.calculate(s, t, p);
         System.out.println(paymentOneYear);
-
-        int paymentTwoYears = (int) service.calculate(1_000_000, 24, 0.0999);
+        s = 1_000_000;
+        t = 24;
+        p = 0.0999;
+        int paymentTwoYears = (int) service.calculate(s, t, p);
         System.out.println(paymentTwoYears);
-
-        int paymentThreeYears = (int) service.calculate(1_000_000, 36, 0.0999);
+        s = 1000000;
+        t = 36;
+        p = 0.0999;
+        int paymentThreeYears = (int) service.calculate(s, t, p);
         System.out.println(paymentThreeYears);
     }
 }
