71b48ce797d2d459f09884cee9ffd6b03d90553c
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-24 20:16:04 +0300

    Delete CreditPaymentService.java

--- src/CreditPaymentService.java
+++
@@ -1,9 +0,0 @@
-public class CreditPaymentService {
-    public double calculate(long s, int t, double p) {
-        double i = Math.pow(1 + (p / 12), t);
-        double k = ((p / 12) * i) / (i - 1);
-        double payment = s * k;
-        return payment;
-    }
-}
-
